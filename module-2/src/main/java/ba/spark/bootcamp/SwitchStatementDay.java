63797c0b1a5b63078c20f94da57d99cb9e2bd2d7
toncicab <bulicic.toncica@nsoft.com>
2019-09-18 11:41:29 +0200

    Add classes for switch statement

---
+++ module-2/src/main/java/ba/spark/bootcamp/SwitchStatementDay.java
@@ -0,0 +1,58 @@
+package ba.spark.bootcamp;
+
+public class SwitchStatementDay {
+    public static void main(String[] args) {
+        int day = 4;
+        String dayString;
+        String dayType;
+
+        switch (day) {
+            case 1:
+                dayString = "Monday";
+                break;
+            case 2:
+                dayString = "Tuesday";
+                break;
+            case 3:
+                dayString = "Wednesday";
+                break;
+            case 4:
+                dayString = "Thursday";
+                break;
+            case 5:
+                dayString = "Friday";
+                break;
+            case 6:
+                dayString = "Saturday";
+                break;
+            case 7:
+                dayString = "Sunday";
+                break;
+            default:
+                dayString = "Invalid day";
+                break;
+        }
+        System.out.println(dayString);
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                dayType = "Weekday";
+                break;
+            case 6:
+            case 7:
+                dayType = "Weekend";
+                break;
+
+            default:
+                dayType = "Invalid day type";
+        }
+
+        System.out.println(dayString + " is a " + dayType);
+    }
+}
+
+
