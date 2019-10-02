5d16c8c83816b31d66e164d88440a6b8ec0b4195
Nikola Rakić <rakic.nikola@nsoft.com>
2019-10-02 22:21:28 +0200

    Add PathVariable name to /helloworld

--- module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
+++ module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
@@ -3,6 +3,7 @@ package ba.spark.bootcamp.helloworld;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,10 @@ public class HelloworldController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @GetMapping("/helloworld/{name}")
+    public ResponseEntity<Greeting> helloworld(@PathVariable("name") String name) {
+        Greeting response = new Greeting(String.format("Hello World, %s!", name));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
