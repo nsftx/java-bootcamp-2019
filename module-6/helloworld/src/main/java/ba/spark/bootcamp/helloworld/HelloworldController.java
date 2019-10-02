b1405e1d5a27ecc36f878953633f9f8f7003653c
Nikola Rakić <rakic.nikola@nsoft.com>
2019-10-02 21:50:48 +0200

    Use GetMapping annotation

--- module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
+++ module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
@@ -1,13 +1,12 @@
 package ba.spark.bootcamp.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldController {
 
-    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    @GetMapping("/helloworld")
     public String helloworld() {
         return "Hello World!";
     }
