1cb228e8adfe6cec15450524ea6303a5560f1675
Nikola Rakić <rakic.nikola@nsoft.com>
2019-10-02 22:16:46 +0200

    Wrap response in ResponseEntity

--- module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
+++ module-6/helloworld/src/main/java/ba/spark/bootcamp/helloworld/HelloworldController.java
@@ -1,5 +1,7 @@
 package ba.spark.bootcamp.helloworld;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldController {
 
     @GetMapping("/helloworld")
-    public Greeting helloworld() {
-        return new Greeting("Hello World!");
+    public ResponseEntity<Greeting> helloworld() {
+        Greeting response = new Greeting("Hello World!");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
